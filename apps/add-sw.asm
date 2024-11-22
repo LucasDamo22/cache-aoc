a7079cdc0734ab3950d7a66a8beead02db829d00
Lucas Damo <lucas.damo@edu.pucrs.br>
2024-11-22 19:57:19 -0300

    changed the program to fill the memory with 'bebacafe'

--- apps/add-sw.asm
+++ apps/add-sw.asm
@@ -3,9 +3,15 @@
 ########################################
 # testes de instrucoes individuais
 ########################################
-main:		
-			addi $s1, $zero, 15
-			sw   $s1, array
-.data
+main:				
+	la   $s2, array
+	lw   $s3, cafe
 
+loop:	lw   $s1, ($s2)
+	addiu $s2, $s2, 4
+	addu $s1, $zero, $s3
+	sw   $s1, 0($s2)
+	j loop
+.data
+cafe: .word 0xBEBACAFE
 array: .word 0xffffffff
